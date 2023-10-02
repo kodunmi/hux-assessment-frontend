4bcc0d362886c8afde5bfeb3484bf591853d0c73
kodunmi <53086177+kodunmi@users.noreply.github.com>
2023-10-02 09:47:28 +0100

    corrected button and input component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-const Button = ({
-  isLoading,
-  children,
-}: {
-  isLoading: boolean;
-  children: React.ReactNode;
-}) => {
-  return <button>{children}</button>;
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
+
+const Button = ({ isLoading, children, type = "button", ...props }: Props) => {
+  return (
+    <button
+      className={`btn btn-primary w-full ${props.className}`}
+      disabled={isLoading}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { UseFormRegister, UseFormRegisterReturn } from "react-hook-form";
 
-const Input = () => {
-  return <input type="text" />;
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  // You can add any additional props or customization you need here
+  register?: UseFormRegisterReturn<any>;
+}
+
+const Input = (props: Props) => {
+  return (
+    <input
+      className={`input w-full ${props.className}`}
+      {...props}
+      {...(props.register && props.register)}
+    />
+  );
 };
 
 export default Input;
